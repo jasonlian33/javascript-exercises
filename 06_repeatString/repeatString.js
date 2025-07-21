9ab89c1af29a453724ac7593ba15e8364a61af79
jason lian <jasonlian16@gmail.com>
2025-07-20 22:56:31 -0400

    completed excercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0){
+        return "ERROR";
+    }
+    let repeat = "";
+    for (let i = 0; i < num; i++){
+        repeat += string;
+    }
+
+    return repeat;
 
 };
 
