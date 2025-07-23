0c48c02fa2760ccd9d8b8ef80a8f081728751bc2
jason lian <jasonlian16@gmail.com>
2025-07-22 23:14:54 -0400

    completed

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let celsius = (degree - 32) * 5/9;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let fahrenheit = (degree * 9/5 + 32);
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
