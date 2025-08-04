1612b795d90ecbf799d6cded5e30c98fbd4846d0
jason lian <jasonlian16@gmail.com>
2025-08-03 22:55:00 -0400

    completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+function removePunctuation(text) {
+    var punctuation = /[\.,?!]/g;
+    var newText = text.replace(punctuation, "");
+    return newText.replaceAll(' ', '').toLowerCase();
+}
 
+// r a c e c a r 
+
+const palindromes = function (string) {
+    let processString = removePunctuation(string);
+    let stringLength = processString.length
+    for (let i = 0; i < stringLength / 2; i ++){
+        if (processString[i] !== processString[stringLength -1 - i]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
